4632c30d395a9fc98b3cafd9f0f97a3f7ecf9825
progtest48-spec <progtest48@gmail.com>
2026-04-23 09:56:20 +0400

    deploying last version

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/urbanDeploy/',
   server: {
     port: 3000,
     host: '0.0.0.0',
